e38cb809bf42d340a996677b65a225d7896ea722
Crissa Owens <crissao@udel.edu>
2024-04-22 16:43:31 -0400

    attatch rings to rod component?

--- src/app/rod/rod.component.ts
+++ src/app/rod/rod.component.ts
@@ -1,10 +1,21 @@
 import { EzComponent } from "@gsilber/webez";
 import html from "./rod.component.html";
 import css from "./rod.component.css";
+import { RingComponent } from "../ring/ring.component";
 
 export class RodComponent extends EzComponent {
+    private rings: RingComponent[] = []
     constructor() {
         super(html, css);
     }
-    
+    setRings(rings: RingComponent[]){
+        this.rings = [];
+        rings.forEach((ring,index) => {
+            this.rings.push(ring);
+            this.addComponent(ring, `ring${index + 1}`);
+        })
+    }
+    getRings(){
+        return this.rings;
+    }
 }
